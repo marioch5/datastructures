ec843349b67dbecb23661ce4fc62037b9be599fe
Pichau <marioa.cherubini@gmail.com>
2025-06-25 13:39:39 -0300

    adds randomized tests to linked list test file

--- typescript/linkedlist/linkedList.spec.ts
+++ typescript/linkedlist/linkedList.spec.ts
@@ -2,7 +2,7 @@ import DoublyLinkedList from "./linkedlist";
 
 describe("LinkedList", () => {
   let list: DoublyLinkedList<number>;
-  const LOOPS = 1000;
+  const LOOPS = 500;
   const TEST_SZ = 40;
   const NUM_NULLS = Math.floor(TEST_SZ / 5);
   const MAX_RAND_NUM = 250;
@@ -163,4 +163,147 @@ describe("LinkedList", () => {
     strs.add("f");
     expect(strs.toString()).toBe("[ a, b, c, d, e, f ]");
   });
+
+  test("randomized indexOf", () => {
+    for (let loops = 0; loops < LOOPS; loops++) {
+      const jsList: number[] = [];
+      list.clear();
+
+      const randNums = genUniqueRandList(TEST_SZ);
+
+      randNums.forEach((val) => {
+        jsList.push(val);
+        list.add(val);
+      });
+
+      shuffle(randNums);
+
+      randNums.forEach((val) => {
+        expect(list.indexOf(val)).toBe(jsList.indexOf(val));
+        expect(list.size).toBe(jsList.length);
+        expect([...list]).toEqual(jsList);
+      });
+    }
+  });
+
+  test("randomized removing", () => {
+    for (let loops = 0; loops < LOOPS; loops++) {
+      const jsList: number[] = [];
+      list.clear();
+
+      const randNums = genRandList(TEST_SZ);
+      for (const value of randNums) {
+        jsList.push(value);
+        list.add(value);
+      }
+
+      shuffle(randNums);
+
+      for (let i = 0; i < randNums.length; i++) {
+        const valToRemove = randNums[i];
+
+        const jsRemoved = removeFirst(jsList, valToRemove);
+        const listRemoved = list.remove(valToRemove);
+
+        expect(jsRemoved).toBe(listRemoved);
+        expect(jsList.length).toBe(list.size);
+        expect([...list]).toEqual(jsList);
+      }
+
+      jsList.length = 0;
+      list.clear();
+
+      for (const value of randNums) {
+        jsList.push(value);
+        list.add(value);
+      }
+
+      for (let i = 0; i < randNums.length; i++) {
+        const rm_val = Math.floor(Math.random() * MAX_RAND_NUM);
+
+        const jsRemoved = removeFirst(jsList, rm_val);
+        const customRemoved = list.remove(rm_val);
+
+        expect(jsRemoved).toBe(customRemoved);
+        expect(jsList.length).toBe(list.size);
+        expect([...list]).toEqual(jsList);
+      }
+    }
+  });
+
+  test("randomized removeAt", () => {
+    for (let loops = 0; loops < LOOPS; loops++) {
+      const jsList: number[] = [];
+      list.clear();
+
+      const randNums = genRandList(TEST_SZ);
+
+      for (const value of randNums) {
+        jsList.push(value);
+        list.add(value);
+      }
+
+      for (let i = 0; i < randNums.length; i++) {
+        const rm_index = Math.floor(Math.random() * list.size);
+
+        const num1 = jsList.splice(rm_index, 1)[0];
+        const num2 = list.removeAt(rm_index);
+
+        expect(num1).toBe(num2);
+        expect(jsList.length).toBe(list.size);
+        expect([...list]).toEqual(jsList);
+      }
+    }
+  });
+
+  function genRandList(size: number): (number | null)[] {
+    const list: (number | null)[] = Array.from({ length: size }, () =>
+      Math.floor(Math.random() * MAX_RAND_NUM)
+    );
+
+    for (let i = 0; i < NUM_NULLS; i++) {
+      list.push(null);
+    }
+
+    shuffle(list);
+
+    return list;
+  }
+
+  function genUniqueRandList(size: number): (number | null)[] {
+    const list: (number | null)[] = Array.from({ length: size }, (_, i) => i);
+
+    for (let i = 0; i < NUM_NULLS; i++) {
+      list.push(null);
+    }
+
+    shuffle(list);
+
+    return list;
+  }
+
+  function removeFirst<T>(arr: T[], value: T): boolean {
+    const index = arr.indexOf(value);
+
+    if (index >= 0) {
+      arr.splice(index, 1);
+      return true;
+    }
+
+    return false;
+  }
+
+  function shuffle(array: any[]) {
+    let currentIndex = array.length;
+
+    while (currentIndex != 0) {
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex],
+        array[currentIndex],
+      ];
+    }
+  }
 });
